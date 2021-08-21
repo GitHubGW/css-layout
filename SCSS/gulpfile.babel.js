38bedf12b15f187db4beda13ea0df206b41d8423
GW <kowonp@gmail.com>
2021-08-21 18:31:02 +0900

    Modified gulpfile.babel file for M1 Mac

--- SCSS/gulpfile.babel.js
+++ SCSS/gulpfile.babel.js
@@ -1,17 +1,21 @@
 import gulp from "gulp";
 import del from "del";
-import sass from "gulp-sass";
+// import sass from "gulp-sass";
 import minify from "gulp-csso";
 import autoprefixer from "gulp-autoprefixer";
 
-sass.compiler = require("node-sass");
+// sass.compiler = require("node-sass"): gulp-sass의 컴파일러를 node-sass로 설정한다는 의미이다.
+// 현재 M1맥에서는 node-sass가 제대로 동작하기 않아서 아래와 같이 코드를 바꿔서 진행했다.
+// 윈도우나 다른환경에서는 위에 gulp-sass를 impmort해주고 node-sass를 불러와서 똑같이 적용하면 된다.
+// sass.compiler = require("node-sass");
+var sass = require("gulp-sass")(require("sass"));
 
 // 모든 경로들을 담은 routes객체를 생성하고 여기서 모든 경로들을 컨트롤 한다.
 const routes = {
   css: {
     watch: "src/scss/*",
     src: "src/scss/styles.scss", // gulpfile은 gulp.src()함수를 통해 지정한 routes.src프로퍼티 안에 있는 값을 컴파일할 파일로 지정한 상태이다.
-    dest: "dist/css", 
+    dest: "dist/css",
   },
 };
 
